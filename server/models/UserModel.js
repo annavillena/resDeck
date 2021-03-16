e39a370e3752e6373249679e945d2a812ecad359
Anna Villena <mavillena@ufl.edu>
2021-03-16 14:31:28 -0400

    editing model for formatting

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose'),
     bcrypt = require('bcrypt-nodejs');
 
-const UserSchema = new mongoose.Schema({
+const User = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
@@ -11,24 +11,22 @@ const UserSchema = new mongoose.Schema({
     date: { type: Date, default: Date.now }
 });
 
-
 // adds method to user to create hashed password
-UserSchema.methods.generateHash = function(password) { //bcrypt function to hash passwords
+User.methods.generateHash = function(password) { //bcrypt function to hash passwords
     return bcrypt.hashSync(password, bcrypt.genSaltSync(8));
 };
 
 // adds method to user to check if password is correct
-UserSchema.methods.validPassword = function(password) {
+User.methods.validPassword = function(password) {
     return bcrypt.compareSync(password, this.password);
 };
 
 // had to add this, checks if password was changed before saving
-UserSchema.pre('save', function(next) { //Prehook
+User.pre('save', function(next) { //Prehook
     if(this.isModified('password')) {
         this.password = this.generateHash(this.password);
     }
     next(); //Continue to next middleware; like save password to database
 });
 
-const User = mongoose.model('user', UserSchema);
-module.exports = User;
+module.exports = mongoose.model('User', User);
