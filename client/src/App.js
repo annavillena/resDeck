cce44e4976bc59f59c7c260420ffe55a94c39185
briankoehler <briankoehler@ufl.edu>
2021-03-18 16:37:39 -0400

    Initial page component setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch, Redirect  } from 'react-router-dom';
 import Home from "./views/Landing/Landing";
+import PersonalProfile from "./views/PersonalProfile/PersonalProfile";
 import NotFound from "./views/NotFound";
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <div>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/profile" component={PersonalProfile} />
         <Route component={NotFound}/>
       </Switch>
     </div>
---
+++ client/src/views/PersonalProfile/PersonalProfile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../components/NavBar/NavBar'
+
+
+const PersonalProfile = () => {
+    return (
+        <Navbar />
+
+        
+    )
+}
+
+export default PersonalProfile
