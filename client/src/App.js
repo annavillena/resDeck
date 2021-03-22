d3505d9e487b51759350e7dc18c075ae29ffe8cf
Anna Villena <mavillena@ufl.edu>
2021-03-22 15:42:44 -0400

    Added routes to app.js for landing and signup

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, Switch, Redirect  } from 'react-router-dom';
 import Home from "./views/Landing/Landing";
 import NotFound from "./views/NotFound";
-import NavBar from "./components/Header/NavBar";
 import SignIn from "./components/SignIn";
 import Feed from "./components/Feed";
 
@@ -11,7 +10,8 @@ const App = () => {
     <div>
    
     <Switch>
-      <Route exact path="/Home" component={SignIn} />
+      <Route exact path="/Home" component={Home} />
+      <Route exact path="/Signup" component={SignIn} />
       <Route exact path="/Feed" component={Feed} />
       <Route exact path="/">
         <Redirect to="/Home" />
