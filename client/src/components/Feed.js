8e2aff8a8ec88a739cefba16cb5a33d529ca479c
briankoehler <briankoehler@ufl.edu>
2021-03-22 22:18:18 -0400

    "Resume change on button clicks"

--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -1,11 +1,17 @@
 import React, {useState} from 'react';
 import './Feed.css';
 import SampleResume from "./sampleRes.jpg"
-import {Button, ButtonGroup, ToggleButton, Radio} from 'react-bootstrap'
-import resume from '../uploads/sample_1.pdf'
+import resume1 from '../uploads/sample_1.pdf'
+import resume2 from '../uploads/sample_2.pdf'
+import resume3 from '../uploads/sample_3.pdf'
+import resume4 from '../uploads/sample_4.pdf'
+import resume5 from '../uploads/sample_5.pdf'
+import resume6 from '../uploads/sample_6.pdf'
 
 const Feed = () => {
-    const [chat, setChat] = useState(false);
+    const [chat, setChat] = useState(false)
+    const [currentResume, setCurrentResume] = useState(0)
+    const resumes = [resume1, resume2, resume3, resume4, resume5, resume6]
 
     // const testFolder = '../uploads/';
 
@@ -28,7 +34,7 @@ const Feed = () => {
 
     function goChat(){
         if(chat)
-            setChat(false)
+        setChat(false)
         else
             setChat(true)
     }
@@ -36,6 +42,24 @@ const Feed = () => {
 
     if(chat){
         return <div>Chat Page!</div>
+    }
+
+    const nextResume = () => {
+        if (currentResume === resumes.length - 1) {
+            setCurrentResume(0)
+        }
+        else {
+            setCurrentResume(currentResume + 1)
+        }
+    }
+
+    const previousResume = () => {
+        if (currentResume === 0) {
+            setCurrentResume(resumes.length - 1)
+        }
+        else {
+            setCurrentResume(currentResume - 1)
+        }
     }
 
 
@@ -68,21 +92,21 @@ const Feed = () => {
                 </svg>
             </div>
 
-            <div className = "like">
+            <div className = "like" onClick={nextResume}>
                 <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <circle cx="40" cy="40" r="38.5" fill="#3F6699" stroke="#3F6699" stroke-width="3"/>
                 <path d="M20 60H24C25.1 60 26 59 26 57.7778V37.7778C26 36.5556 25.1 35.5556 24 35.5556H20V60ZM59.66 44.1778C59.88 43.6222 60 43.0222 60 42.4V40C60 37.5556 58.2 35.5556 56 35.5556H45L46.84 25.2222C46.94 24.7333 46.88 24.2 46.68 23.7556C46.22 22.7556 45.64 21.8444 44.92 21.0444L44 20L31.18 34.2444C30.42 35.0889 30 36.2222 30 37.4V54.8222C30 57.6667 32.1 60 34.68 60H50.9C52.3 60 53.62 59.1778 54.34 57.8444L59.66 44.1778Z" fill="white"/>
                 </svg>
             </div>
 
-            <div className = "rewind">
+            <div className = "rewind" onClick={previousResume}>
                 <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <circle cx="40" cy="40" r="38.5" fill="#78C2C3" stroke="#78C2C3" stroke-width="3"/>
                 <path d="M25.8787 25.875C29.5059 22.25 34.4841 20 40.0125 20C51.0694 20 60 28.95 60 40C60 51.05 51.0694 60 40.0125 60C30.6817 60 22.9018 53.625 20.6754 45H25.8787C27.93 50.825 33.4834 55 40.0125 55C48.2927 55 55.0219 48.275 55.0219 40C55.0219 31.725 48.2927 25 40.0125 25C35.8599 25 32.1576 26.725 29.4559 29.45L37.5109 37.5H20V20L25.8787 25.875Z" fill="white"/>
                 </svg>
             </div>
 
-            <div className = "skip">
+            <div className = "skip" onClick={nextResume}>
                 <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <circle cx="40" cy="40" r="38.5" fill="#3F6699" stroke="#3F6699" stroke-width="3"/>
                 <path d="M42.25 55.9584L62.5 40.5L42.25 25.0417V55.9584Z" fill="white" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
@@ -90,7 +114,7 @@ const Feed = () => {
                 </svg>
             </div>
 
-            <div className = "save">
+            <div className = "save" onClick={nextResume}>
                 <svg width="84" height="82" viewBox="0 0 84 82" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M82.5 41C82.5 62.7814 64.4018 80.5 42 80.5C19.5982 80.5 1.5 62.7814 1.5 41C1.5 19.2186 19.5982 1.5 42 1.5C64.4018 1.5 82.5 19.2186 82.5 41Z" fill="#78C2C3" stroke="#78C2C3" stroke-width="3"/>
                 <rect width="54" height="61" transform="translate(15 12)" fill="#78C2C3"/>
@@ -98,7 +122,7 @@ const Feed = () => {
                 </svg>
             </div>
 
-            <object data={resume + '#toolbar=0&navpanes=0&scrollbar=0'} type="application/pdf" className='pdf'></object>
+            <object data={resumes[currentResume] + '#toolbar=0&navpanes=0&scrollbar=0'} type="application/pdf" className='pdf'></object>
 
         </div>
     )
